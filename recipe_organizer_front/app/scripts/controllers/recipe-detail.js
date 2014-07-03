4962543b76c93d5ba5df7e91e256ef31f619e4ef
Greg Burdge <codingcorner@outlook.com>
2014-07-03 10:39:14 -0600

    "added delete functionality and logic for it"

--- recipe_organizer_front/app/scripts/controllers/recipe-detail.js
+++ recipe_organizer_front/app/scripts/controllers/recipe-detail.js
@@ -8,12 +8,23 @@
  * Controller of the djangularFrontendApp
  */
 angular.module('djangularFrontendApp')
-    .controller('RecipeDetailCtrl', function ($scope, $routeParams, Restangular) {
+    .controller('RecipeDetailCtrl', function ($scope, $routeParams, Restangular, $location) {
         $scope.recipeId = $routeParams.recipeId;
 
         Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
             $scope.recipe = data;
-
         });
+
+        $scope.deleteRecipe = function () {
+            var response = confirm("Are you sure you want to delete this recipe?");
+            if (response == true) {
+                Restangular.one('recipes', $scope.recipeId
+            ).customDELETE().then(function () {
+                    $location.path('/recipes');
+                }, function () {
+                    $scope.status = "The recipe couldn't be deleted";
+                });
+            }
+        };
     });
 ;
